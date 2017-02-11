cb9a9553165911437c968f2e72806f22eb7f630a
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-02-11 18:17:33 -0500

    added post

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ entended: false }));
 
 db.sync().then(db.seed)
     .then(() => db.Story.getStoriesByAuthor('poke gui'))
@@ -15,6 +18,18 @@ app.get('/tag', (req, res) => {
 app.get('/:name?', (req, res) => {
     db.Story.getStoriesByAuthor(req.params.name)
         .then(stories => res.json(stories.map(str => str.title)));
+});
+
+
+app.post('/', (req, res) => {
+    const authorName = req.body.name;
+    const title = req.body.title;
+    const content = req.body.content;
+    const tags = req.body.tags;
+
+    db.Story.createStory(authorName, { title, content, tags }).then(() => res.redirect(303, `/${authorName}`));
+
+
 });
 
 const port = process.env.PORT || 3000;
