90d88c026eacf558b65a3dd86a23d6c477014268
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-02-11 18:03:50 -0500

    refactor get by tag

--- db/index.js
+++ db/index.js
@@ -2,10 +2,12 @@ const Sequelize = require('sequelize');
 
 const db = new Sequelize(process.env.DATABASE_URL);
 
+//makes users table. Also ensure we have an id, createdAt, updatedAt
 const User = db.define('user', {
     name: Sequelize.STRING
 });
 
+//makes stories table. with id, createdAt, updatedAt
 const Story = db.define('story', {
     content: Sequelize.STRING,
     title: Sequelize.STRING,
@@ -17,11 +19,16 @@ const Story = db.define('story', {
         }
     },
 }, {
+    // adds methods to story that makes our life easier
     classMethods: {
+        //will lookup and create a user if needed before getting stories
+        //{title:title, content:content, tags:tags}
         createStory: (authorName, { title, content, tags }) => User.findOne({ where: { name: authorName } })
             .then(user => user ? user : User.create({ name: authorName }))
             .then(author => Story.create({ content, title, tags, userId: author.id })),
-        getStories: (authorName) => {
+
+        //function that allows us to pull users and filter by name if present
+        getStoriesByAuthor: (authorName) => {
             let filter = {};
             if (authorName) {
                 filter.name = authorName;
@@ -32,25 +39,39 @@ const Story = db.define('story', {
                     where: filter
                 }]
             });
+        },
+        getStoriesByTag: (tag) => {
+            console.log(tag);
+            let options = {
+                where: { tags: { $contains: [tag] } },
+                include: [User]
+            };
+            return Story.findAll(options);
+
         }
     }
 });
 
+//tells relationship. Users can have many stories and a story has 1 user. adds userId to stories.
 Story.belongsTo(User);
 User.hasMany(Story);
 
 let connection;
 
+//connects to db
 const connect = () => {
     connection = connection || db.authenticate();
     return connection;
 };
 
+// makes inital data. Adds a user poke. then adds 3 stories and adds an addtional user faitzi
 const seed = () => connect()
     .then(() => User.create({ name: 'poke gui' }))
     .then(author => Story.createStory('poke gui', { title: 'sexzia', content: 'boobah' }))
     .then(author => Story.createStory('poke gui', { title: 'yazu', content: 'fompe', tags: 'abc, xyz' }))
     .then(author => Story.createStory('faitzi', { title: 'danni is cool', content: 'super cool', tags: 'abc' }));
+
+//deletes tables and ensure tables = models we have set up
 const sync = () => connect().then(() => db.sync({ force: true }));
 
 module.exports = { db, seed, sync, User, Story };
--- server.js
+++ server.js
@@ -3,21 +3,18 @@ const app = express();
 const db = require('./db');
 
 db.sync().then(db.seed)
-    .then(() => db.Story.getStories('poke gui'))
+    .then(() => db.Story.getStoriesByAuthor('poke gui'))
     .then(stuff => console.log(stuff[0].title, 1234));
 
 
 app.get('/tag', (req, res) => {
-    let options = {
-        where: { tags: { $contains: [req.query.tag] } },
-        include: [db.User]
-    };
-    db.Story.findAll(options).then(stories => res.json(stories.map(str => str.title)));
+
+    db.Story.getStoriesByTag(req.query.tag).then(stories => res.json(stories.map(str => str.title)));
 });
 
 app.get('/:name?', (req, res) => {
-    db.Story.getStories(req.params.name)
-        .then(stories => res.json(stories.map(str => str.user.name)));
+    db.Story.getStoriesByAuthor(req.params.name)
+        .then(stories => res.json(stories.map(str => str.title)));
 });
 
 const port = process.env.PORT || 3000;
