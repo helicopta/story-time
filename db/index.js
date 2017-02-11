f3aecc73d76aa20fb22e26fed770b1ea558f7eec
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-02-11 17:08:55 -0500

    added story model and seed

--- db/index.js
+++ db/index.js
@@ -6,6 +6,27 @@ const User = db.define('user', {
     name: Sequelize.STRING
 });
 
+const Story = db.define('story', {
+    content: Sequelize.STRING,
+    title: Sequelize.STRING,
+    tags: {
+        type: Sequelize.ARRAY(Sequelize.STRING),
+        set: function(val) {
+            let tags = val ? val.split(',').map(tag => tag.trim()) : null;
+            this.setDataValue('tags', tags);
+        }
+    },
+}, {
+    classMethods: {
+        createStory: (authorName, { title, content, tags }) => User.findOne({ where: { name: authorName } })
+            .then(user => user ? user : User.create({ name: authorName }))
+            .then(author => Story.create({ content, title, tags, userId: author.id }))
+    }
+});
+
+Story.belongsTo(User);
+User.hasMany(Story);
+
 let connection;
 
 const connect = () => {
@@ -13,8 +34,11 @@ const connect = () => {
     return connection;
 };
 
-const seed = () => connect().then(() => User.create({ name: 'poke gui' }));
-
+const seed = () => connect()
+    .then(() => User.create({ name: 'poke gui' }))
+    .then(author => Story.createStory('poke gui', { title: 'sexzia', content: 'boobah' }))
+    .then(author => Story.createStory('poke gui', { title: 'yazu', content: 'fompe', tags: '1,2,3,4,5' }))
+    .then(author => Story.createStory('faitzi', { title: 'danni is cool', content: 'super cool', tags: '1,2,3' }));
 const sync = () => connect().then(() => db.sync({ force: true }));
 
 module.exports = { db, seed, sync, User };
